a969967f010f13615cea94767939e530de96dc51
Loshan Parajuli <loshanparajuli@192.168.137.159>
2025-10-13 21:09:48 +0545

    wip-optimised for build

--- src/app/gabriel/page.tsx
+++ src/app/gabriel/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const page = () => {
+export default function PageF() {
   return (
-    <div>Gabriel&apos;s Page</div>
-  )
+    <div className="w-full h-screen overflow-hidden">
+      <iframe
+        src="https://www.canva.com/design/DAG1XSfZyyg/2AeAxJ8MuEkHpLY4l3AZ4g/view?embed"
+        className="w-full h-full border-0"
+        allowFullScreen
+        title="Case Studey"
+      />
+    </div>
+  );
 }
 
-export default page
